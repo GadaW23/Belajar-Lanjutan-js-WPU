50deab87c859779596911bbcbc021532fdd3589c
unknown <gadawanada4@gmail.com>
2023-01-16 11:20:05 +0700

    update belajar

--- script.js
+++ script.js
@@ -19,20 +19,26 @@
 // }
 
 //2.function Declration
-// function Mahasiswa(nama, energi) {
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
-
-//     mahasiswa.makan = function(porsi){
+// const methodMahasiswa = {
+//     makan : function(porsi){
 //         this.energi += porsi;
 //         console.info(`Halo ${this.nama}, selamat makan`);
-//     }
+//     },
 
-//     mahasiswa.main = function (jam) {
+//     main : function (jam) {
 //         this.energi -= jam;
-//         console.info(`Halo  ${this.nama}, selamat main`)
+//         console.info(`Halo  ${this.nama}, selamat main`);
+//     },
+
+//     tidur : function (jam){
+//         this.energi += jam * 2;
+//         console.info(`Halo ${this.nama}, Selamat Tidur`);
 //     }
+// }
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
 
 //     return mahasiswa;
 // }
@@ -40,22 +46,51 @@
 // let Gada = Mahasiswa('Gada', 10);
 // let Ghatvaan = Mahasiswa('Ghatvaan', 10);
 
-//3.
-function Mahasiswa(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
 
-    this.makan = function(porsi){
-        this.energi += porsi;
-        console.info(`Halo ${this.nama}, selamat makan`);
+//3. Contraktor Function
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+
+//     this.makan = function(porsi){
+//         this.energi += porsi;
+//         console.info(`Halo ${this.nama}, selamat makan`);
+//     }
+
+//     this.main = function (jam) {
+//         this.energi -= jam;
+//         console.info(`Halo  ${this.nama}, selamat main`)
+//     }
+
+// }
+
+// let gada = new Mahasiswa("Gada Wanada", 10);
+// let ghina = new Mahasiswa("Ghina husna", 10);
+
+
+// Versi class
+
+class Mahasiswa {
+    constructor(nama, energi){
+        this.nama = nama;
+        this.energi = energi;
     }
 
-    this.main = function (jam) {
-        this.energi -= jam;
-        console.info(`Halo  ${this.nama}, selamat main`)
-    }
+             makan = function(porsi){
+                this.energi += porsi;
+                console.info(`Halo ${this.nama}, selamat makan`);
+            }
+        
+            main = function (jam) {
+                this.energi -= jam;
+                console.info(`Halo  ${this.nama}, selamat main`)
+            }
+
+            tidur = function(jam){
+                this.energi += jam * 2;
+                console.info(`Halo ${this.nama}, selamat tidur`)
+            }
 
 }
 
 let gada = new Mahasiswa("Gada Wanada", 10);
-let ghina = new Mahasiswa("Ghina husna", 10);
